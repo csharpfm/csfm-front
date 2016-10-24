49bc74e20baa3cbd396bda1a218691dfeb2cf594
Pierre Dfc <piierre.d@live.fr>
2016-10-25 00:40:35 +0200

    bottom bar

--- csfm-android/csfm-android/MainActivity.cs
+++ csfm-android/csfm-android/MainActivity.cs
@@ -2,14 +2,20 @@
 using Android.Widget;
 using Android.OS;
 using Android.Views;
+using System;
+using BottomNavigationBar;
+using Android.Support.V7.App;
+using Android.Support.V4.Content;
 
 namespace csfm_android
 {
     [Activity(Label = "csfm_android", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    public class MainActivity : AppCompatActivity
     {
 
         private Toolbar toolbar;
+
+        private BottomBar bottomBar;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -21,12 +27,34 @@ namespace csfm_android
             this.toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
             SetActionBar(toolbar);
             ActionBar.Title = "MatchFM";
+
+            bottomBar = BottomBar.Attach(this, bundle);
+
+            BottomBarTab[] tabs = new BottomBarTab[5];
+            tabs[0] = new BottomBarTab(Resource.Drawable.ic_home_white_24dp, "Home");
+            tabs[1] = new BottomBarTab(Resource.Drawable.ic_menu_search_holo_dark, "Search");
+            tabs[2] = new BottomBarTab(Resource.Drawable.ic_favorite_white_24dp, "Match");
+            tabs[3] = new BottomBarTab(Resource.Drawable.ic_chat_white_24dp, "Chat");
+            tabs[4] = new BottomBarTab(Resource.Drawable.ic_account_box_white_24dp, "Account");
+
+            bottomBar.SetItems(tabs);
+            for(int i = 0; i < 5; i++)
+            {
+                bottomBar.MapColorForTab(i, "#F44336");
+            }
+
+            
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
         {
             MenuInflater.Inflate(Resource.Menu.top_menus, menu);
             return base.OnCreateOptionsMenu(menu);
+        }
+
+        public void OnMenuItemSelected(int menuItemId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
