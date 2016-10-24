b9f847e0e6ddb4bc6b15fc099f7dbd48e941a238
Pierre Dfc <piierre.d@live.fr>
2016-10-24 23:58:07 +0200

    Initial commit

---
+++ csfm-android/csfm-android/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace csfm_android
+{
+    [Activity(Label = "csfm_android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+
+        Toolbar toolbar;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            this.toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetActionBar(toolbar);
+            ActionBar.Title = "MatchFM";
+        }
+    }
+}
+
