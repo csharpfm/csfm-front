4fc0c7c7c1d1729ff61b30feabd6fff99a835c77
Pierre Dfc <piierre.d@live.fr>
2016-10-31 23:20:27 +0100

    Match design improvements

--- csfm-android/csfm-android/Fragments/HomeFragment.cs
+++ csfm-android/csfm-android/Fragments/HomeFragment.cs
@@ -44,7 +44,7 @@ namespace csfm_android.Fragments
             artist.Name = "Lady Gaga";
             album.Artist = artist;
             album.Name = "Joanne";
-            album.Image = "https://coverartarchive.org/release-group/13afa2b3-60d3-4cfd-b590-d12b4774876b/front-250.jpg";
+            album.Image = "https://lh4.googleusercontent.com/-p1ejPKmyA2s/AAAAAAAAAAI/AAAAAAACRXU/6S-Em-MWl08/s0-c-k-no-ns/photo.jpg";
             Track track1 = new Track();
             track1.Album = album;
             track1.Name = "Perfect Illusion";
