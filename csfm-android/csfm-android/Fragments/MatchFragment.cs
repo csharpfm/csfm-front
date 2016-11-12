c08b2fed1641228bb294dcaf4c9abae6a5b71652
Pierre Dfc <piierre.d@live.fr>
2016-11-12 18:48:52 +0100

    Match handler

--- csfm-android/csfm-android/Fragments/MatchFragment.cs
+++ csfm-android/csfm-android/Fragments/MatchFragment.cs
@@ -15,6 +15,8 @@ using Square.Picasso;
 using csfm_android.Api.Model;
 using csfm_android.Activities;
 using csfm_android.Utils.MaterialDesignSearchView;
+using csfm_android.Api;
+using csfm_android.Utils;
 
 namespace csfm_android.Fragments
 {
@@ -31,7 +33,9 @@ namespace csfm_android.Fragments
 
         private Button loadMore;
 
-        private LinkedList<User> recommendedUsers;
+        private List<User> recommendedUsers;
+
+        private User currentUser;
 
         public override void OnCreate(Bundle savedInstanceState)
         {
@@ -68,30 +72,55 @@ namespace csfm_android.Fragments
 
             this.InitButtons();
 
-            this.recommendedUsers = new LinkedList<User>();
+            this.recommendedUsers = new List<User>();
 
-            User user1 = new User();
-            user1.Username = "Hugoatease";
-            user1.Photo = "https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/14492433_730273613793013_3473639481244418470_n.jpg?oh=8a6bcee3852f9dfa67e95155fe336209&oe=5889175E";
-            User user2 = new User();
-            user2.Username = "Clément de Chereng";
-            user2.Photo = "https://scontent-cdg2-1.xx.fbcdn.net/v/t34.0-0/s261x260/14971176_10210410328633000_797219556_n.jpg?oh=92361adb492252d0c756e24ae9349a6e&oe=58229A8A";
+            /*  User user1 = new User();
+              user1.Username = "Hugoatease";
+              user1.Photo = "https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/14492433_730273613793013_3473639481244418470_n.jpg?oh=8a6bcee3852f9dfa67e95155fe336209&oe=5889175E";
+              User user2 = new User();
+              user2.Username = "Clément de Chereng";
+              user2.Photo = "https://scontent-cdg2-1.xx.fbcdn.net/v/t34.0-0/s261x260/14971176_10210410328633000_797219556_n.jpg?oh=92361adb492252d0c756e24ae9349a6e&oe=58229A8A";
 
-            this.recommendedUsers.AddLast(user1);
-            this.recommendedUsers.AddLast(user2);
+              this.recommendedUsers.AddLast(user1);
+              this.recommendedUsers.AddLast(user2);*/
+
+            GetMatch();
 
             this.likeButton.Click += delegate
             {
-                //TODO LIKE API 
-                this.Next();
+                if (this.currentUser != null)
+                {
+                    PutMatch(true);
+                    this.Next();
+                } 
             };
 
             this.nextButton.Click += delegate
             {
-                this.Next();
+                if (this.currentUser != null)
+                {
+                    PutMatch(false);
+                    this.Next();
+                }
             };
 
-            this.Next();
+            this.loadMore.Click += delegate
+            {
+                GetMatch();
+                if (this.recommendedUsers != null)
+                {
+                    this.Next();
+                }
+            };
+
+            if (this.recommendedUsers.Any())
+            {
+                this.Next();
+            }
+            else
+            {
+                LoadMore();
+            }
         }
 
 
@@ -123,11 +152,26 @@ namespace csfm_android.Fragments
             return updatedBitmap;
         }
 
+        private async void GetMatch()
+        {
+            var users = await new ApiClient().GetUserMatch(CSFMPrefs.Prefs.GetString(CSFMApplication.Username, ""));
+
+            this.recommendedUsers = users;
+            Console.WriteLine(users);
+        }
+
+        private async void PutMatch(bool isMatch)
+        {
+            await new ApiClient().PutUserMatch(CSFMPrefs.Prefs.GetString(CSFMApplication.Username, ""), currentUser.Id, isMatch);
+        }
+
         private void Next()
         {
             if (this.recommendedUsers.Any())
             {
                 User user = this.recommendedUsers.First();
+
+                this.currentUser = user;
 
                 Picasso.With(this.Activity)
                     .Load(user.Photo)
@@ -136,20 +180,25 @@ namespace csfm_android.Fragments
                 this.username.Text = user.Username;
                 this.favoriteSong.Text = "Mylène Farmer - Libertine"; // TODO
 
-                this.recommendedUsers.RemoveFirst();
+                this.recommendedUsers.Remove(user);
             }
             else
             {
-                this.avatar.SetImageResource(0);
-                this.username.Text = "";
-                this.favoriteSong.Text = "";
-
-                this.loadMore.Visibility = ViewStates.Visible;
-                // TODO LOAD MORE
+                LoadMore();
             }
 
         }
 
+        private void LoadMore()
+        {
+            this.avatar.SetImageResource(0);
+            this.username.Text = "";
+            this.favoriteSong.Text = "";
+
+            this.currentUser = null;
+
+            this.loadMore.Visibility = ViewStates.Visible;
+        }
     }
    
 }
